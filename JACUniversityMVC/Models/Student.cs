f91f64c23de5a5de34d08eb3df4ab869e93a12ee
rezasaleh <37808126+rezasaleh@users.noreply.github.com>
2020-05-30 01:25:44 +0430

    JACUniversity

---
+++ JACUniversityMVC/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JACUniversityMVC.Models
+{
+   
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        [NotMapped]
+        public string FullName => this.FirstName + " " + this.LastName;
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
